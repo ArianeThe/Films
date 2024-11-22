f786f9c7d86cc1cb07cbf6b1091b3a087e460b70
ArianeThe <ariane.thebault2022@campus-eni.fr>
2024-11-22 12:49:49 +0100

    MAJ

---
+++ films/src/main/java/com/films/films/Entities/User.java
@@ -0,0 +1,15 @@
+package com.films.films.Entities;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+
+    private int No_Users;
+
+    private String User_Name;
+
+    private String User_pseudo;
+
+    private String User_MotDePasse;
+    
+}
